547f53247cc65a98204607c931c18279f954d77c
Dhananjay98-lang <dhananjay_bankar_comp@moderncoe.edu.in>
2025-08-15 17:58:44 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,7 +1,29 @@
 public class Day1{
   public static void main(String args[]){
     int array[] = {0,1,2,1,0,2,1,0};
-    int n = array.length();
-    System.out.println(n);
+    int n = array.length;
+    int start = 0, end = n-1;
+    for(int i=0; i<n; i++){
+      if(array[i]==0){
+        array[i]=array[i]+array[start];
+        array[start]=array[i]-array[start];
+        array[i]=array[i]-array[start];
+        start++;
+      }
+    }
+
+     for(int i=0; i<n; i++){
+      if(array[i]==2){
+        array[i]=array[i]+array[end];
+        array[end]=array[i]-array[end];
+        array[i]=array[i]-array[end];
+        end--;
+      }
+    }
+
+    for(int i=0;i<n-1;i++){
+      System.out.print(array[i]+", ");
+    }
+    System.out.print(array[n-1]);
   }
 }
